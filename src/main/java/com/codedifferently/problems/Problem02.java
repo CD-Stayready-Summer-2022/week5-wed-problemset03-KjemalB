dff6b132db31897309b4ad1af8fd44c11f05728d
Khalil Brewington <kbrewing02@gmail.com>
2022-07-06 15:54:01 -0400

    problem 2 3:53

--- src/main/java/com/codedifferently/problems/Problem02.java
+++ src/main/java/com/codedifferently/problems/Problem02.java
@@ -4,7 +4,18 @@ import java.util.ArrayList;
 
 public class Problem02 {
     public static Integer cousinTag(ArrayList<String> cousins, Integer startIndex, String targetName){
+        int tagGapRight = 0;
+        int tagGapLeft = 0;
+        int targetIndex = cousins.indexOf(targetName);
 
-        return null;
+        tagGapLeft = startIndex - targetIndex;
+        tagGapRight = ((cousins.size() - 1) - startIndex) + (targetIndex + 1);
+        tagGapLeft = Math.abs(tagGapLeft);
+        tagGapRight = Math.abs(tagGapRight);
+
+        if (tagGapLeft < tagGapRight) {
+            return tagGapLeft;
+        }else
+            return tagGapRight;
     }
 }
